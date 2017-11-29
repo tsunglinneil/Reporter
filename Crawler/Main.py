4282906180e2fcdb231c524f55a91129a2f9f872
tsunglinken <magic3657@gmail.com>
2017-11-29 22:21:03 +0800

    create and use leveldb

--- Crawler/Main.py
+++ Crawler/Main.py
@@ -1,14 +1,36 @@
+# coding=utf-8
 # import class
 import sys
-from Crawler import Reporter, Utils
+from Crawler import Reporter, Utils, Leveldbutil
+from datetime import datetime
+
+# Hint doc:
+# datetime in python : https://docs.python.org/3/library/datetime.html#datetime-objects
+# datetime strftime() and strptime() : https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
 
 # ============== Main ==============
 
+db = Leveldbutil.init("nbaDB")  # get database object
 URL = "http://sports.ltn.com.tw/nba"
 report = Reporter.reporter(Reporter.get_nba_report(URL))
-print(sys.path)
+# print(sys.path)
 if report:
-    # create FileUtils object
-    fileUtil = Utils.FileUtils()
-    # call object method
-    fileUtil.generate_sound(report)
+    #  以下單純以當前時間作為key(記錄到秒) 收集每一次取到的即時賽事結果
+    timeForm = "%Y-%m-%d %H:%M:%S"
+    today = datetime.today().strftime(timeForm)
+
+    # 寫入DB
+    Leveldbutil.insert(db, today, report)
+
+    # 從DB取得此次即時賽事資訊並匯出音檔
+    try:
+        # create FileUtils object
+        fileUtil = Utils.FileUtils()
+        # call object method
+        fileUtil.generate_sound(Leveldbutil.search(db, today))
+    except:
+        print("No data found : {}".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
+
+    # 查看DB所有資料
+    Leveldbutil.dump(db)
+
