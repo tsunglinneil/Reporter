750482fb3813bbccbfb40b479024bb6a0cf0898a
tsunglinken <magic3657@gmail.com>
2017-12-14 00:24:58 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'NBAReporter',
+    scripts = ['Main'],
+    version = '1.0',
+    description = 'Get NBA competition data',
+    author = 'tsunglin.ken',
+    keywords = ['NBA Reporter'],
+    classifiers = [],
+)
